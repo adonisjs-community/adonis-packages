23df19ac7f3d5ec1bba74a91301d6b84ab6a9b62
Julien Ripouteau <julien@ripouteau.com>
2023-11-17 16:06:50 +0100

    feat: polish package card

--- resources/components/package_card.vue
+++ resources/components/package_card.vue
@@ -18,10 +18,19 @@ function iconPlaceholder({ category }: PackageInfo) {
 </script>
 
 <template>
-  <div class="card flex rounded-xl gap-y-2 px-5 py-5">
-    <Tag>{{ package.category }}</Tag>
+  <a
+    class="card flex rounded-xl gap-y-2 px-5 py-5 group cursor-pointer"
+    :href="package.website || package.github"
+    target="_blank"
+  >
+    <div class="flex items-center justify-between w-full">
+      <Tag>{{ package.category }}</Tag>
+      <i
+        class="inline-block i-icon-park-outline-share text-xs text-white-300 opacity-0 group-hover:opacity-100 transition duration-500 hover:text-white"
+      />
+    </div>
     <div class="h-12 w-12 mt-4 items-center justify-center">
-      <img v-if="package.icon" class="h-full" :src="`/icons/${package.icon}`" />
+      <img v-if="package.icon" class="h-full object-contain" :src="`/icons/${package.icon}`" />
       <div
         v-else
         style="background: rgba(43, 43, 43, 0.23)"
@@ -39,20 +48,40 @@ function iconPlaceholder({ category }: PackageInfo) {
       </p>
     </div>
 
-    <div class="flex text-md text-white-300 gap-5 mt-4">
-      <div class="flex items-center gap-3">
+    <div class="flex text-md text-white-300 gap-6 mt-4">
+      <a
+        class="flex items-center gap-1.3 transition-colors duration-600 hover:text-white"
+        :href="package.github"
+        target="_blank"
+      >
         <i class="inline-block i-fluent-emoji-star" />
         <span class="text-sm relative top-0.4">{{ package.stars }} stars</span>
-      </div>
+      </a>
 
-      <div class="flex items-center gap-3">
+      <a
+        class="flex items-center gap-1.3 transition-colors duration-600 hover:text-white"
+        :href="`https://www.npmjs.com/package/${package.npm}`"
+        target="_blank"
+      >
         <i class="inline-block i-fluent-emoji-chart-increasing" />
-        <span class="text-sm relative top-0.4"
-          >{{ numberFormatter(package.downloads) }} installs</span
-        >
-      </div>
+        <span class="text-sm relative top-0.4">
+          {{ numberFormatter(package.downloads) }} installs
+        </span>
+      </a>
     </div>
-  </div>
+  </a>
 </template>
 
-<style scoped></style>
+<style scoped lang="postcss">
+.card {
+  background: #171717;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  transition: all 0.2s ease-in-out;
+  border: 1px solid transparent;
+  &:hover {
+    border: 1px solid rgba(140, 140, 140, 0.1);
+  }
+}
+</style>
