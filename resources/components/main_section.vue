c926f1e7de71870def9c64135e1dbf4d99b5a9ab
Julien Ripouteau <julien@ripouteau.com>
2023-11-17 15:40:07 +0100

    refactor: extract package card component

--- resources/components/main_section.vue
+++ resources/components/main_section.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import { millify } from 'millify'
 import { computed } from 'vue'
 import Fuse from 'fuse.js'
+import PackageCard from './package_card.vue'
 import type { PackageInfo, PackagesFilters } from '@/types'
-import Tag from '@/components/tag.vue'
-import { categories } from '~/content/categories'
 
 const props = defineProps<{
   packages: PackageInfo[]
@@ -21,14 +19,6 @@ const fuseOptions = {
 
 const fuseIndex = Fuse.createIndex(fuseOptions.keys, props.packages)
 const fuse = new Fuse(props.packages, fuseOptions, fuseIndex)
-
-function numberFormatter(num: number) {
-  return millify(num || 0, { precision: 1 })
-}
-
-function iconPlaceholder({ category }: PackageInfo) {
-  return categories.find((c) => c.label === category)?.icon || 'i-fluent-emoji-package-24-regular'
-}
 
 const filteredPackages = computed(() => {
   let packages = props.packages
@@ -55,45 +45,7 @@ const filteredPackages = computed(() => {
 
 <template>
   <section class="grid grid-cols-3 gap-5">
-    <div
-      v-for="pkg in filteredPackages"
-      :key="pkg.name"
-      class="card flex rounded-xl gap-y-2 px-5 py-5"
-    >
-      <Tag>{{ pkg.category }}</Tag>
-      <div class="h-12 w-12 mt-4 items-center justify-center">
-        <img v-if="pkg.icon" class="h-full" :src="`/icons/${pkg.icon}`" />
-        <div
-          v-else
-          style="background: rgba(43, 43, 43, 0.23)"
-          class="h-full flex items-center justify-center rounded-xl overflow-hidden"
-        >
-          <i class="inline-block bg-grey text-lg" :class="iconPlaceholder(pkg)" />
-        </div>
-      </div>
-      <div class="flex flex-col gap-y-1">
-        <p class="text-2xl font-bold">
-          {{ pkg.name }}
-        </p>
-        <p class="text-white-300 text-sm line-clamp-3">
-          {{ pkg.description }}
-        </p>
-      </div>
-
-      <div class="flex text-md text-white-300 gap-5 mt-4">
-        <div class="flex items-center gap-3">
-          <i class="inline-block i-fluent-emoji-star" />
-          <span class="text-sm relative top-0.4">{{ pkg.stars }} stars</span>
-        </div>
-
-        <div class="flex items-center gap-3">
-          <i class="inline-block i-fluent-emoji-chart-increasing" />
-          <span class="text-sm relative top-0.4"
-            >{{ numberFormatter(pkg.downloads) }} installs</span
-          >
-        </div>
-      </div>
-    </div>
+    <PackageCard v-for="pkg in filteredPackages" :key="pkg.name" :package="pkg" />
   </section>
 </template>
 
--- resources/components/package_card.vue
+++ resources/components/package_card.vue
@@ -1,7 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { millify } from 'millify'
+import type { PackageInfo } from '@/types'
+import { categories } from '~/content/categories'
+import Tag from '@/components/tag.vue'
+
+defineProps<{
+  package: PackageInfo
+}>()
+
+function numberFormatter(num: number) {
+  return millify(num || 0, { precision: 1 })
+}
+
+function iconPlaceholder({ category }: PackageInfo) {
+  return categories.find((c) => c.label === category)?.icon || 'i-fluent-emoji-package-24-regular'
+}
+</script>
 
 <template>
-  <div />
+  <div class="card flex rounded-xl gap-y-2 px-5 py-5">
+    <Tag>{{ package.category }}</Tag>
+    <div class="h-12 w-12 mt-4 items-center justify-center">
+      <img v-if="package.icon" class="h-full" :src="`/icons/${package.icon}`" />
+      <div
+        v-else
+        style="background: rgba(43, 43, 43, 0.23)"
+        class="h-full flex items-center justify-center rounded-xl overflow-hidden"
+      >
+        <i class="inline-block bg-grey text-lg" :class="iconPlaceholder(package)" />
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-1">
+      <p class="text-2xl font-bold">
+        {{ package.name }}
+      </p>
+      <p class="text-white-300 text-sm line-clamp-3">
+        {{ package.description }}
+      </p>
+    </div>
+
+    <div class="flex text-md text-white-300 gap-5 mt-4">
+      <div class="flex items-center gap-3">
+        <i class="inline-block i-fluent-emoji-star" />
+        <span class="text-sm relative top-0.4">{{ package.stars }} stars</span>
+      </div>
+
+      <div class="flex items-center gap-3">
+        <i class="inline-block i-fluent-emoji-chart-increasing" />
+        <span class="text-sm relative top-0.4"
+          >{{ numberFormatter(package.downloads) }} installs</span
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
