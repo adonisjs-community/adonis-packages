f31c52a4b0d27e23e500f1f85287c40dc4f2ca9d
Julien Ripouteau <julien@ripouteau.com>
2023-11-19 02:01:11 +0100

    chore: minor styling changes

--- resources/components/header.vue
+++ resources/components/header.vue
@@ -5,7 +5,7 @@ import Socials from '@/components/socials.vue'
 </script>
 
 <template>
-  <header class="border-b border-b-gray/10 backdrop-blur sticky top-0 z-10">
+  <header class="border-b border-b-gray/4 backdrop-blur sticky top-0 z-10">
     <div class="p-container py-4 flex justify-between w-full items-center">
       <Link href="/" :preserve-scroll="true" :preserve-state="true">
         <AdonisLogo />
--- resources/pages/home/components/search_bar.vue
+++ resources/pages/home/components/search_bar.vue
@@ -15,13 +15,13 @@ watch(debounced, (value) => emit('update:modelValue', value))
 <template>
   <div class="relative">
     <i
-      class="absolute top-[14px] left-[12px] inline-block i-fluent-emoji-magnifying-glass-tilted-right"
+      class="absolute top-[14px] left-[18px] inline-block i-fluent-emoji-magnifying-glass-tilted-right"
     />
     <input
       v-model="input"
       type="text"
       placeholder="Search for a package"
-      class="search-bar-input w-full border-0 bg-transparent py-3 px-4 pl-10 rounded-xl focus:(ring-0 outline-0)"
+      class="search-bar-input w-full border-0 bg-transparent py-3 px-6 pl-12 rounded-xl focus:(ring-0 outline-0)"
     />
   </div>
 </template>
