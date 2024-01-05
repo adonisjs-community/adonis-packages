e39dfe1ab77e43a0c81d8a809368b26088a469c2
Julien Ripouteau <julien@ripouteau.com>
2024-01-05 12:08:35 +0100

    refactor: refactor order with defineModel

--- resources/pages/home/components/order.vue
+++ resources/pages/home/components/order.vue
@@ -1,22 +1,7 @@
 <script lang="ts" setup>
-import { inject, ref } from 'vue'
+import { inject } from 'vue'
 
-const props = defineProps<{
-  modelValue: -1 | 1
-}>()
-
-const emits = defineEmits<{
-  'update:modelValue': [-1 | 1]
-}>()
-
-const order = ref<-1 | 1>(props.modelValue)
-
-const toggleOrder = () => {
-  order.value = order.value === 1 ? -1 : 1
-
-  emits('update:modelValue', order.value)
-}
-
+const order = defineModel<1 | -1>()
 const group = inject<boolean>('group', false)
 </script>
 
@@ -25,7 +10,7 @@ const group = inject<boolean>('group', false)
     class="flex items-center px-3 py-2 text-base12 shadow-sm focus:(outline-0 ring-0)"
     :class="{ 'bg-base2 rounded-xl': !group }"
     :aria-label="order === 1 ? 'Sort by descending order' : 'Sort by ascending order'"
-    @click="toggleOrder"
+    @click="order = order === 1 ? -1 : 1"
   >
     <i :class="order === 1 ? 'i-carbon-arrow-up' : 'i-carbon-arrow-down'" class="inline-block" />
   </button>
