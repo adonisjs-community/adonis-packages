cfa9104e85e2d9b00c96c6fc11b82c18eeb765a3
barbapapazes <e.soubiran25@gmail.com>
2024-01-06 13:03:05 +0100

    feat: better support for mobile

---
+++ resources/pages/home/components/filters/filter_categories.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import SelectMenu from '../select_menu.vue'
+
+defineProps<{
+  categories: {
+    value: string
+    label: string
+  }[]
+}>()
+
+const category = defineModel<string>({
+  required: true,
+})
+</script>
+
+<template>
+  <SelectMenu v-model="category" :options="categories" placeholder="Select a category" />
+</template>
