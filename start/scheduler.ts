14531596197f4879a5dda0fc955a64d471195247
Julien Ripouteau <julien@ripouteau.com>
2024-05-18 09:54:50 +0200

    fix: dont run scheduler if not in production
    Otherwise, the scheduler will run every time the server is restarted, and so the boot time will explode.
    
    In development, we should instead use seeders. The PackageStatSeeder seeder is responsible for fetching all this.

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -1,7 +1,6 @@
 import { CronJob } from 'cron'
 import app from '@adonisjs/core/services/app'
 
-import env from '#start/env'
 import { PackagesDataRefresher } from '#services/packages_data_refresher'
 
 /**
@@ -12,5 +11,5 @@ CronJob.from({
   start: true,
   cronTime: '0 */3 * * *',
   onTick: () => refresher.refresh(),
-  runOnInit: !!env.get('GITHUB_TOKEN') || app.inProduction,
+  runOnInit: app.inProduction,
 })
