a6dbe80f3e85e03c7c84cc5582d5918c8f353b9b
Julien Ripouteau <julien@ripouteau.com>
2025-06-09 03:36:59 +0200

    feat: cool anim filters

--- inertia/pages/home/components/filters.vue
+++ inertia/pages/home/components/filters.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { motion } from 'motion-v'
+
 import FilterButton from './filter_button.vue'
 import type { PackageCategories, PackageCategory } from '@/app/types'
 
@@ -11,12 +13,16 @@ const categoryModel = defineModel<PackageCategory | null>('category')
 
 <template>
   <div class="hidden flex-col gap-4" md="flex">
-    <FilterButton
+    <motion.div
       v-for="category in props.categories"
       :key="category.label"
-      :item="category"
-      :is-selected="categoryModel === category.label"
-      @click="categoryModel = categoryModel === category.label ? null : category.label"
-    />
+      :while-hover="{ x: 10, transition: { type: 'spring', stiffness: 800, damping: 17 } }"
+    >
+      <FilterButton
+        :item="category"
+        :is-selected="categoryModel === category.label"
+        @click="categoryModel = categoryModel === category.label ? null : category.label"
+      />
+    </motion.div>
   </div>
 </template>
