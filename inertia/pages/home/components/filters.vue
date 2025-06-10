c6e9c0507bb4ae74544b50d18ed9c9af7653501b
Julien Ripouteau <julien@ripouteau.com>
2025-06-11 00:21:10 +0200

    feat: re-add version selector

--- inertia/pages/home/components/filters.vue
+++ inertia/pages/home/components/filters.vue
@@ -9,20 +9,56 @@ const props = defineProps<{
 }>()
 
 const categoryModel = defineModel<PackageCategory | null>('category')
+const versionModel = defineModel<string | null>('version')
+
+const adonisVersions = [
+  {
+    label: 'AdonisJS 6',
+    value: '6',
+    icon: 'i-mynaui:six-waves',
+    subline: 'Compatible with AdonisJS 6',
+  },
+  {
+    label: 'AdonisJS 5',
+    value: '5',
+    icon: 'i-mynaui:five-waves',
+    subline: 'Compatible with AdonisJS 5',
+  },
+]
 </script>
 
 <template>
   <div class="hidden flex-col gap-4" md="flex">
-    <motion.div
-      v-for="category in props.categories"
-      :key="category.label"
-      :while-hover="{ x: 10, transition: { type: 'spring', stiffness: 800, damping: 17 } }"
-    >
-      <FilterButton
-        :item="category"
-        :is-selected="categoryModel === category.label"
-        @click="categoryModel = categoryModel === category.label ? null : category.label"
-      />
-    </motion.div>
+    <div>
+      <div class="flex flex-col gap-2">
+        <motion.div
+          v-for="version in adonisVersions"
+          :key="version.value"
+          :while-hover="{ x: 10, transition: { type: 'spring', stiffness: 800, damping: 17 } }"
+        >
+          <FilterButton
+            :item="version"
+            :is-selected="versionModel === version.value"
+            @click="versionModel = versionModel === version.value ? null : version.value"
+          />
+        </motion.div>
+      </div>
+    </div>
+
+    <div class="mt-6">
+      <div class="flex flex-col gap-2">
+        <motion.div
+          v-for="category in props.categories"
+          :key="category.label"
+          :while-hover="{ x: 10, transition: { type: 'spring', stiffness: 800, damping: 17 } }"
+        >
+          <FilterButton
+            :item="category"
+            :is-selected="categoryModel === category.label"
+            @click="categoryModel = categoryModel === category.label ? null : category.label"
+          />
+        </motion.div>
+      </div>
+    </div>
   </div>
 </template>
